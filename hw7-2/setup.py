7a627866ff7562c7a22fba8b9dd095dfdd3042e1
Volodymyr Kurov <vokur13@gmail.com>
2023-05-04 17:55:05 +0300

    setup.py +

---
+++ hw7-2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="clean_folder",
+    version="1",
+    description="Very useful Clean Your Folder code",
+    url="https://github.com/GoIT-Python/goit-python-core-hw7-2",
+    author="Volodymyr Kurov",
+    author_email="vokur13@gmail.com",
+    license="MIT",
+    include_package_data=True,
+    packages=find_namespace_packages(),
+    install_requires=["transliterate"],
+    entry_points={"console_scripts": ["clean-folder = clean_folder.clean:sort"]},
+)
---
+++ hw7-2/test.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+path = Path("./bin")
+
+print(path.absolute())
